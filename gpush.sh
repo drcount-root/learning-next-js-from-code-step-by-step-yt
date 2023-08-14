81beda3c9adc391b6dea234a95f0de64262562fc
Subham Sahu <about.subhamsahu@gmail.com>
2023-08-14 20:00:18 +0530

    gpush

--- gpush.sh
+++ gpush.sh
@@ -1,48 +1,32 @@
 #!/bin/bash
 
-# ANSI escape codes for colors
-RED="\033[0;31m"
-GREEN="\033[0;32m"
-YELLOW="\033[0;33m"
-CYAN="\033[0;36m"
-RESET="\033[0m"  # Reset color
-
-# Function to print colored text
-print_colored() {
-    local color="$1"
-    shift
-    echo -e "${color}$@${RESET}"
-}
-
-# Function to prompt for input and check if empty
-get_non_empty_input() {
-    local prompt="$1"
-    local input
-    while true; do
-        read -p "$(print_colored ${GREEN}${prompt}${RESET}) " input
-        if [[ -z "$input" ]]; then
-            print_colored ${RED}"Message cannot be empty. Please enter some message."${RESET}
-        else
-            echo "$input"
-            break
-        fi
-    done
-}
-
 git add .
 
-message=$(get_non_empty_input "Add Commit message:")
+while true; do
+    # Take message
+    read -p "Add Commit message: " message
 
-git commit -m "$message"
-git push -u origin main
+    # Check if message is empty
+    if [[ -z "$message" ]]; then
+        echo "Message cannot be empty. Please enter some message."
+    else
+        git commit -m "$message"
+        git push -u origin main
+        break
+    fi
+done
 
-print_colored ${CYAN}"OK! ✅"${RESET}
+echo "OK! ✅"
+
+colors=("31" "32" "33" "34" "35")  # Different colors for each line (red, green, yellow, blue, purple)
 
 for i in 5 4 3 2 1; do
-    print_colored ${YELLOW}"Exiting in $i seconds..."${RESET}
+    echo -e "\e[${colors[$i-1]}mExiting in $i seconds...\e[0m"
     sleep 1
 done
 
+
+###################### OLD SCRIPT ######################
 
 # #!/bin/bash
 

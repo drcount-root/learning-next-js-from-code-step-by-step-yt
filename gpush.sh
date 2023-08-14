11e8869063bfcfb9e7824f16248ef88c02ae6fc4
Subham Sahu <about.subhamsahu@gmail.com>
2023-08-14 20:05:34 +0530

    .

--- gpush.sh
+++ gpush.sh
@@ -3,12 +3,10 @@
 git add .
 
 while true; do
-    # Take message
     read -p "Add Commit message: " message
 
-    # Check if message is empty
     if [[ -z "$message" ]]; then
-        echo "Message cannot be empty. Please enter some message."
+        echo -e "\e[1;31mError occurred, message can't be empty\e[0m"
     else
         git commit -m "$message"
         git push -u origin main
@@ -18,7 +16,7 @@ done
 
 echo "OK! ✅"
 
-colors=("31" "32" "33" "34" "35")  # Different colors for each line (red, green, yellow, blue, purple)
+colors=("34" "32" "33" "31" "35")
 
 for i in 5 4 3 2 1; do
     echo -e "\e[${colors[$i-1]}mExiting in $i seconds...\e[0m"
@@ -26,7 +24,38 @@ for i in 5 4 3 2 1; do
 done
 
 
-###################### OLD SCRIPT ######################
+
+######################################################################################
+# V2
+
+# #!/bin/bash
+
+# git add .
+
+# while true; do
+#     read -p "Add Commit message: " message
+
+#     if [[ -z "$message" ]]; then
+#         echo "Message cannot be empty. Please enter some message."
+#     else
+#         git commit -m "$message"
+#         git push -u origin main
+#         break
+#     fi
+# done
+
+# echo "OK! ✅"
+
+# colors=("34" "32" "33" "31" "35")
+
+# for i in 5 4 3 2 1; do
+#     echo -e "\e[${colors[$i-1]}mExiting in $i seconds...\e[0m"
+#     sleep 1
+# done
+
+
+##################################### OLD SCRIPT #####################################
+# V1
 
 # #!/bin/bash
 

c4c87cdaff072355ce01148ad5744c7400eb361f
Subham Sahu <about.subhamsahu@gmail.com>
2023-08-15 17:28:58 +0530

    Conditional Layout

--- src/app/login/layout.jsx
+++ src/app/login/layout.jsx
@@ -1,23 +1,31 @@
+"use client";
 import Link from "next/link";
-import './login.css'
+import "./login.css";
+import { usePathname } from "next/navigation";
+
 export default function Layout({ children }) {
-    return (
-        <div>
-            <ul className="login-menu">
-                <li>
-                    <h4>Login Navbar</h4>
-                </li>
-            <li>
-                <Link href="/login">Login Main</Link>
-            </li>
-            <li>
-                <Link href="/login/loginstudent">Student Login </Link>
-            </li>
-            <li>
-                <Link href="/login/loginteacher">Teacher Login </Link>
-            </li>
-            </ul>
-            {children}
-        </div>
-    )
+  const currentPathname = usePathname();
+  console.log(currentPathname);
+
+  return (
+    <div>
+      {currentPathname !== "/login/loginteacher" ? (
+        <ul className="login-menu">
+          <li>
+            <h4>Login Navbar</h4>
+          </li>
+          <li>
+            <Link href="/login">Login Main</Link>
+          </li>
+          <li>
+            <Link href="/login/loginstudent">Student Login </Link>
+          </li>
+          <li>
+            <Link href="/login/loginteacher">Teacher Login </Link>
+          </li>
+        </ul>
+      ) : <Link href="/login">Go to main login page</Link>}
+      {children}
+    </div>
+  );
 }

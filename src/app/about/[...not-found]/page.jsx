e43dc2cdc52489d0538554589d8e1db63984fec0
Subham Sahu <about.subhamsahu@gmail.com>
2023-08-16 20:30:43 +0530

    "404 - Global Page & Nest Pages

---
+++ src/app/about/[...not-found]/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const AboutPageNotFound = () => {
+  return (
+    <div>
+      <h2>
+        Inside About Route There Is No Nested Route Names What You Are Trying To
+        Find
+      </h2>
+    </div>
+  );
+};
+
+export default AboutPageNotFound;

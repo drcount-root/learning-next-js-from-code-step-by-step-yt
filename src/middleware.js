0a2a478af2d0e40477a54e0282546fdcf9b3acf9
Subham Sahu <about.subhamsahu@gmail.com>
2023-08-17 22:30:35 +0530

    middleware with config

--- src/middleware.js
+++ src/middleware.js
@@ -12,5 +12,24 @@ export const config = {
   matcher: ["/about/:path*", "/studentlist/:page*", "/study/:path*"],
 };
 
-// Now the about config sets - 
+// Now the about config sets -
 // if the requested endpoint is equal to - /about, /about/anythihng, /studentlist, /studentlist/anything, /study, /study/anything then, those routes should immediately redirected to /login route.
+
+// ###############################################
+
+// trying to access any nested route of /about should redirect to /login
+
+// import { NextResponse } from "next/server";
+
+// export function middleware(request) {
+//     const { pathname } = request.nextUrl;
+
+//     // Check if the current page is any nested page under /about
+//     if (pathname.startsWith("/about/")) {
+//         return NextResponse.redirect(new URL("/login", request.url));
+//     }
+// }
+
+// export const config = {
+//     matcher: '/about/:path*',
+// };

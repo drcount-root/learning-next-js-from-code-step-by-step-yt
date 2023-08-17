3c445ef8d9731417d978d875cc369409ae331cf4
Subham Sahu <about.subhamsahu@gmail.com>
2023-08-17 22:02:01 +0530

    middleware with config

--- src/middleware.js
+++ src/middleware.js
@@ -1,8 +1,16 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
-  console.log("middleware executed");
-  if (request.nextUrl.pathname != "/login") {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
+  // console.log("middleware executed");
+  // if (request.nextUrl.pathname != "/login") {
+  // the above logic is about, if the requested endpoint is not equal to /login, then redirect to /login
+  return NextResponse.redirect(new URL("/login", request.url));
+  // }
 }
+
+export const config = {
+  matcher: ["/about/:path*", "/studentlist/:page*", "/study/:path*"],
+};
+
+// Now the about config sets - 
+// if the requested endpoint is equal to - /about, /about/anythihng, /studentlist, /studentlist/anything, /study, /study/anything then, those routes should immediately redirected to /login route.

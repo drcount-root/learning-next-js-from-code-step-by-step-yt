b0073a723a4f52fdd29d6a54b9346d1b8b5fc47a
Subham Sahu <about.subhamsahu@gmail.com>
2023-08-18 23:28:50 +0530

    SSR

---
+++ src/app/ssrexample/page.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const dataFetch = async () => {
+  const response = await fetch(
+    "https://dummyjson.com/products?limit=10&skip=10&select=title,price"
+  );
+  const data = response.json();
+  return data;
+};
+
+const SSRExample = async () => {
+  const showableData = await dataFetch();
+  console.log(showableData.products);
+  return (
+    <div>
+        <h2>SSR : API DATA</h2>
+      {showableData.products.map((i) => {
+        return <p key={i.id}>{i.title}</p>
+      })}
+    </div>
+  );
+};
+
+export default SSRExample;

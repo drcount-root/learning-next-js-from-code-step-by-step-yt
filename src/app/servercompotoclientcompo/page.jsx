b5eb0cef80e50754486716ffdf6a7175b8e66ca0
Subham Sahu <about.subhamsahu@gmail.com>
2023-08-19 19:21:34 +0530

    Passing data from Server Component to Client Component

---
+++ src/app/servercompotoclientcompo/page.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Product from "./product";
+
+const dataFetch = async () => {
+  const response = await fetch(
+    "https://dummyjson.com/products?limit=10&skip=10&select=title,price"
+  );
+  const data = response.json();
+  console.log(data.products);
+  return data;
+};
+
+const SSRExample = async () => {
+  const showableData = await dataFetch();
+  console.log(showableData.products);
+  return (
+    <div>
+      <h2>SSR : API DATA</h2>
+      {showableData.products.map((i) => (
+        <div key={i.id}>
+          <p>{i.title}</p>
+          <Product price={i.price} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SSRExample;

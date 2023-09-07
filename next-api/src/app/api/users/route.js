8f2a6c35a4f30516cfa6ce959ecdda123067046f
Subham Sahu <about.subhamsahu@gmail.com>
2023-09-07 20:06:42 +0530

    POST API route with static data

--- next-api/src/app/api/users/route.js
+++ next-api/src/app/api/users/route.js
@@ -5,3 +5,19 @@ export function GET() {
   const data = users;
   return NextResponse.json(data, { status: 200 });
 }
+
+export async function POST(request) {
+  let payload = await request.json();
+  console.log(payload.name);
+  if (!payload.name || !payload.age || !payload.email) {
+    return NextResponse.json(
+      { result: "Require field not found", success: false },
+      { status: 400 }
+    );
+  }
+
+  return NextResponse.json(
+    { result: "New user created", success: true },
+    { status: 201 }
+  );
+}

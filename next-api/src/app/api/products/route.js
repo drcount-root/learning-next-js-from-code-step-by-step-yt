26c1aacef8ddeafc974377494ef328021c43d68a
Subham Sahu <about.subhamsahu@gmail.com>
2023-09-16 20:29:23 +0530

    POST API of adding new product

--- next-api/src/app/api/products/route.js
+++ next-api/src/app/api/products/route.js
@@ -1,7 +1,9 @@
-import mongoose from "mongoose";
+import mongoose, { ConnectionStates } from "mongoose";
 import { NextResponse } from "next/server";
 import { connectionSrt } from "../../../lib/db";
 import { Product } from "../../../lib/model/product";
+
+// GET Products
 
 export async function GET() {
   let data = [];
@@ -14,4 +16,15 @@ export async function GET() {
   }
 
   return NextResponse.json({ result: data, success: true });
+}
+
+// POST
+
+export async function POST(request) {
+  const payload = await request.json();
+  await mongoose.connect(connectionSrt);
+  let product = new Product(payload);
+
+  const newProduct = await product.save();
+  return NextResponse.json({ result: newProduct, status: true });
 }
